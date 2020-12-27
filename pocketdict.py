69912c9283c34b87db6904fda423efb04aecfc6a
Shivansh Agarwal <32426700+Shivansh001@users.noreply.github.com>
2020-12-28 00:24:21 +0530

    Add files via upload

---
+++ pocketdict.py
@@ -0,0 +1,9 @@
+#to make a pocket dictonary in python we can use
+#pyDictionary
+#pip install Dictionary
+
+from PyDictionary import PyDictionary
+
+dictionary = PyDictionary()
+
+print(dictionary.meaning(input("Enter the keyword: ")))
